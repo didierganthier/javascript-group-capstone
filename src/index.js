508233026df51406f9282cd0ffafe077c9250033
Didier Peran Ganthier <didierganthierperan@gmail.com>
2022-10-20 13:35:47 -0400

    Add likes functionality

--- src/index.js
+++ src/index.js
@@ -1,6 +1,54 @@
 /* eslint-disable camelcase */
 import '../dist/output.css';
+import getLikes from './modules/getLikes.js';
 import init from './modules/init.js';
 
 // Initialize the app
 init();
+
+// create app using Involvement API and console.log the response
+// const createApp = async () => {
+//   const response = await fetch('https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/', {
+//     method: 'POST',
+//     body: JSON.stringify({
+//       name: 'My App',
+//     }),
+//     headers: {
+//       'Content-type': 'application/json; charset=UTF-8',
+//     },
+//   });
+//   const data = await response.text();
+//   console.log(data);
+// };
+
+// createApp();
+
+// l1xzt8yaktrcb6minzZP
+
+// Add like to game using Involvement API and console.log the response
+const addLike = async (id) => {
+  fetch('https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/AvZcLNYpo6DEiCsZuxdL/likes/', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      item_id: id,
+    }),
+  }).then(() => {
+    getLikes();
+  });
+};
+
+document.querySelector('#events').addEventListener('DOMSubtreeModified', () => {
+  document.querySelectorAll('svg').forEach((svg) => {
+    if (svg.id.includes('like')) {
+      svg.addEventListener('click', (e) => {
+        addLike(e.target.id);
+        e.target.style = 'fill: #f00';
+      });
+    }
+  });
+});
+
+getLikes();
--- src/modules/createEventHtml.js
+++ src/modules/createEventHtml.js
@@ -1,8 +1,7 @@
-/* eslint-disable camelcase */
 // Create the HTML for the event
 const createEventHtml = (event) => {
   const {
-    title, type, short_title, venue, id,
+    title, type, venue, id,
   } = event;
   const eventHtml = `
   <div class="w-full max-w-sm bg-white rounded-xl shadow-2xl m-10 bg-opacity-25 backdrop-filter backdrop-blur-lg">
@@ -29,11 +28,11 @@ const createEventHtml = (event) => {
           `
 }
       <div class="flex">
-         <svg aria-label="Like" class="ml-4" color="#262626" fill="#262626" height="24" role="img" viewBox="0 0 24 24" width="24"><path d="M16.792 3.904A4.989 4.989 0 0 1 21.5 9.122c0 3.072-2.652 4.959-5.197 7.222-2.512 2.243-3.865 3.469-4.303 3.752-.477-.309-2.143-1.823-4.303-3.752C5.141 14.072 2.5 12.167 2.5 9.122a4.989 4.989 0 0 1 4.708-5.218 4.21 4.21 0 0 1 3.675 1.941c.84 1.175.98 1.763 1.12 1.763s.278-.588 1.11-1.766a4.17 4.17 0 0 1 3.679-1.938m0-2a6.04 6.04 0 0 0-4.797 2.127 6.052 6.052 0 0 0-4.787-2.127A6.985 6.985 0 0 0 .5 9.122c0 3.61 2.55 5.827 5.015 7.97.283.246.569.494.853.747l1.027.918a44.998 44.998 0 0 0 3.518 3.018 2 2 0 0 0 2.174 0 45.263 45.263 0 0 0 3.626-3.115l.922-.824c.293-.26.59-.519.885-.774 2.334-2.025 4.98-4.32 4.98-7.94a6.985 6.985 0 0 0-6.708-7.218Z"></path></svg>
-         <svg aria-label="Comment" class="ml-4" height="24" role="img" viewBox="0 0 24 24" width="24"><path d="M20.656 17.008a9.993 9.993 0 1 0-3.59 3.615L22 22Z" fill="none" stroke="currentColor" stroke-linejoin="round" stroke-width="2"></path></svg>
+         <svg aria-label="Like" id="like-${id}" class="ml-4 relative like" color="#262626" fill="#262626" height="24" role="img" viewBox="0 0 24 24" width="24"><path d="M16.792 3.904A4.989 4.989 0 0 1 21.5 9.122c0 3.072-2.652 4.959-5.197 7.222-2.512 2.243-3.865 3.469-4.303 3.752-.477-.309-2.143-1.823-4.303-3.752C5.141 14.072 2.5 12.167 2.5 9.122a4.989 4.989 0 0 1 4.708-5.218 4.21 4.21 0 0 1 3.675 1.941c.84 1.175.98 1.763 1.12 1.763s.278-.588 1.11-1.766a4.17 4.17 0 0 1 3.679-1.938m0-2a6.04 6.04 0 0 0-4.797 2.127 6.052 6.052 0 0 0-4.787-2.127A6.985 6.985 0 0 0 .5 9.122c0 3.61 2.55 5.827 5.015 7.97.283.246.569.494.853.747l1.027.918a44.998 44.998 0 0 0 3.518 3.018 2 2 0 0 0 2.174 0 45.263 45.263 0 0 0 3.626-3.115l.922-.824c.293-.26.59-.519.885-.774 2.334-2.025 4.98-4.32 4.98-7.94a6.985 6.985 0 0 0-6.708-7.218Z"></path></svg>
+         <svg aria-label="Comment" id="comment-${id}" class="ml-4 relative comment" height="24" role="img" viewBox="0 0 24 24" width="24"><path d="M20.656 17.008a9.993 9.993 0 1 0-3.59 3.615L22 22Z" fill="none" stroke="currentColor" stroke-linejoin="round" stroke-width="2"></path></svg>
       </div>
       <div class="ml-4 my-2">
-          <p>Liked by <span class="font-bold">${short_title}</span> and others</p>
+          <p id="liked-by-${id}" class="like-count">Be the first to like this</p>
           <p>This is some dummy text, don't mind it</p>
           <p class="text-blue-300">#${type}</p>
       </div>
